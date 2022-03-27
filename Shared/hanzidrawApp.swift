1d5a2e01bb841bbd9db685ec183c704d1103be15
RImac Valdez <rimacq@protonmail.com>
2022-03-27 01:18:20 +0100

    reduce log level to errors in revenue cat. Set Constants.AppVersion on init

--- Shared/hanzidrawApp.swift
+++ Shared/hanzidrawApp.swift
@@ -12,6 +12,7 @@ import RevenueCat
 struct Constants {
     static var drawPadSize = CGFloat(200)
     static var adFrequency : Int = 3
+    static var appVersion = "0.0.1"
 }
 
 @main
@@ -19,8 +20,17 @@ struct hanzidrawApp: App {
     let adsVM = AdsViewModel.shared
     init(){
         GADMobileAds.sharedInstance().start(completionHandler: nil)
-        Purchases.logLevel = .debug
+        Purchases.logLevel = .error
         Purchases.configure(withAPIKey: "appl_BODvJYsoznPWIKVhHRVTbRqcZFd")
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"]
+        if(appVersion != nil && appVersion is String){
+            Constants.appVersion = appVersion as! String
+            let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"]
+            if(buildNumber != nil && buildNumber is String){
+                Constants.appVersion += "."
+                Constants.appVersion += buildNumber as! String
+            }
+        }
     }
   
     var body: some Scene {
